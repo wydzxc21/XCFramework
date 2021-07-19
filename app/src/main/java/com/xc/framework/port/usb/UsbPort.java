13399842f7235fccdfa578d8e6c3664e040ae26b
zhangxuanchen <394171231@qq.com>
2021-07-19 14:31:16 +0800

    修改串口相关参数命名

--- app/src/main/java/com/xc/framework/port/usb/UsbPort.java
+++ app/src/main/java/com/xc/framework/port/usb/UsbPort.java
@@ -32,7 +32,7 @@ public class UsbPort implements IPort {
     //
     private UsbBaseDriver mDriver;
     private UsbDeviceConnection mUc;
-    private int sendTimeout;//发送超时，默认1000
+    private int sendTimeout;//发送超时，默认2000
 
     /**
      * Author：ZhangXuanChen
@@ -87,7 +87,7 @@ public class UsbPort implements IPort {
      * @param dataBits    数据位，默认8
      * @param stopBits    停止位，默认1
      * @param parity      奇偶校验位，默认0（无校验）
-     * @param sendTimeout 发送超时，默认1000
+     * @param sendTimeout 发送超时，默认2000
      */
     public boolean openPort(UsbDevice usbDevice, int baudRate, int dataBits, int stopBits, int parity, int sendTimeout) {
         this.sendTimeout = sendTimeout;
