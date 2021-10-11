6fe1eb4001030cfff5ffc7839000ddd1865e4c57
zhangxuanchen <394171231@qq.com>
2021-10-11 10:13:42 +0800

    扩展XCTimeUtil

--- app/src/main/java/com/xc/framework/util/XCTimeUtil.java
+++ app/src/main/java/com/xc/framework/util/XCTimeUtil.java
@@ -31,6 +31,10 @@ public class XCTimeUtil {
      */
     public static final String FORMAT_DATE_SECOND = "yyyy-MM-dd HH:mm:ss";
     /**
+     * 日期格式（yyyyMMddHHmmss）
+     */
+    public static final String FORMAT_DATE_SECOND_NUMBER = "yyyyMMddHHmmss";
+    /**
      * 日期格式（yyyy-MM-dd HH:mm:ss.SSS）
      */
     public static final String FORMAT_DATE_MSEC = "yyyy-MM-dd HH:mm:ss.SSS";
@@ -50,6 +54,16 @@ public class XCTimeUtil {
      * 时差-北京
      */
     public static final String TIME_ZONE_BEIJING = "GMT+:08:00";
+
+    /**
+     * 获取当前时间
+     *
+     * @param simpleDateFormat 时间格式
+     * @return 时间格式字符串
+     */
+    public static String getCurrentTime(SimpleDateFormat simpleDateFormat) {
+        return getTime(System.currentTimeMillis(), simpleDateFormat);
+    }
 
     /**
      * 获取当前时间
@@ -94,11 +108,24 @@ public class XCTimeUtil {
      * @return 时间格式字符串
      */
     public static String getTime(long milliseconds, String dateFormat, String timeZone) {
+        return getTime(milliseconds, getSimpleDateFormat(dateFormat, timeZone));
+    }
+
+    /**
+     * 获取时间
+     *
+     * @param milliseconds     时间戳
+     * @param simpleDateFormat 时间格式
+     * @return 时间格式字符串
+     */
+    public static String getTime(long milliseconds, SimpleDateFormat simpleDateFormat) {
         try {
             if (milliseconds < 0) {
                 milliseconds = System.currentTimeMillis();
             }
-            SimpleDateFormat simpleDateFormat = getSimpleDateFormat(dateFormat, timeZone);
+            if (simpleDateFormat == null) {
+                simpleDateFormat = getSimpleDateFormat(null);
+            }
             return simpleDateFormat.format(new Date(milliseconds));
         } catch (Exception e) {
             e.printStackTrace();
@@ -121,11 +148,23 @@ public class XCTimeUtil {
      * Description：获取日期
      */
     public static Date getDate(String dateStr, String dateFormat, String timeZone) {
+        return getDate(dateStr, getSimpleDateFormat(dateFormat, timeZone));
+    }
+
+    /**
+     * Author：ZhangXuanChen
+     * Time：2020/4/2 13:58
+     * Description：获取日期
+     */
+    public static Date getDate(String dateStr, SimpleDateFormat simpleDateFormat) {
         try {
             if (XCStringUtil.isEmpty(dateStr)) {
-                dateStr = getCurrentTime(dateFormat);
+                dateStr = getCurrentTime(FORMAT_DATE);
             }
-            return getSimpleDateFormat(dateFormat, timeZone).parse(dateStr);
+            if (simpleDateFormat == null) {
+                simpleDateFormat = getSimpleDateFormat(FORMAT_DATE);
+            }
+            return simpleDateFormat.parse(dateStr);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -138,7 +177,7 @@ public class XCTimeUtil {
      * @return 例:星期一
      */
     public static String getCurrentWeek() {
-        return getWeek(getCurrentTime(null));
+        return getWeek(getCurrentTime(FORMAT_DATE));
     }
 
     /**
@@ -148,7 +187,7 @@ public class XCTimeUtil {
      * @return 例:星期一
      */
     public static String getWeek(String date) {
-        return getWeek(date, null);
+        return getWeek(date, "");
     }
 
     /**
@@ -158,11 +197,23 @@ public class XCTimeUtil {
      * @return 例:星期一
      */
     public static String getWeek(String date, String timeZone) {
+        return getWeek(date, getSimpleDateFormat(FORMAT_DATE, timeZone));
+    }
+
+    /**
+     * 获取星期
+     *
+     * @param date 日期格式
+     * @return 例:星期一
+     */
+    public static String getWeek(String date, SimpleDateFormat simpleDateFormat) {
         String week = "";
         try {
-            SimpleDateFormat format = getSimpleDateFormat(FORMAT_DATE, timeZone);
+            if (simpleDateFormat == null) {
+                simpleDateFormat = getSimpleDateFormat(FORMAT_DATE);
+            }
             Calendar calendar = Calendar.getInstance();
-            calendar.setTime(format.parse(date));
+            calendar.setTime(simpleDateFormat.parse(date));
             int weekKey = calendar.get(Calendar.DAY_OF_WEEK);
             switch (weekKey) {
                 case 1:
@@ -199,14 +250,23 @@ public class XCTimeUtil {
      * Description：比较相差天数
      */
     public static int compareDay(String beforeDate, String afterDate, String dateFormat) {
+        return compareDay(beforeDate, afterDate, getSimpleDateFormat(dateFormat));
+    }
+
+    /**
+     * Author：ZhangXuanChen
+     * Time：2019/12/11 11:39
+     * Description：比较相差天数
+     */
+    public static int compareDay(String beforeDate, String afterDate, SimpleDateFormat simpleDateFormat) {
         int day = 0;
         try {
-            if (XCStringUtil.isEmpty(dateFormat)) {
-                dateFormat = FORMAT_DATE;
+            if (simpleDateFormat == null) {
+                simpleDateFormat = getSimpleDateFormat(FORMAT_DATE);
             }
-            SimpleDateFormat sdf = getSimpleDateFormat(dateFormat, null);
-            day = compareDay(sdf.parse(beforeDate), sdf.parse(afterDate));
+            day = compareDay(simpleDateFormat.parse(beforeDate), simpleDateFormat.parse(afterDate));
         } catch (ParseException e) {
+            e.printStackTrace();
         }
         return day;
     }
@@ -221,6 +281,7 @@ public class XCTimeUtil {
         try {
             day = compareDay(beforeDate.getTime(), afterDate.getTime());
         } catch (Exception e) {
+            e.printStackTrace();
         }
         return day;
     }
@@ -235,19 +296,28 @@ public class XCTimeUtil {
         try {
             day = ((afterMillisecond - beforeMillisecond) / (1000L * 3600L * 24L));
         } catch (Exception e) {
+            e.printStackTrace();
         }
         return (int) day;
     }
-
 
     /**
      * Author：ZhangXuanChen
      * Time：2020/7/18 14:36
      * Description：getSimpleDateFormat
      */
-    private static SimpleDateFormat getSimpleDateFormat(String dateFormat, String timeZone) {
+    public static SimpleDateFormat getSimpleDateFormat(String dateFormat) {
+        return getSimpleDateFormat(dateFormat, null);
+    }
+
+    /**
+     * Author：ZhangXuanChen
+     * Time：2020/7/18 14:36
+     * Description：getSimpleDateFormat
+     */
+    public static SimpleDateFormat getSimpleDateFormat(String dateFormat, String timeZone) {
         if (XCStringUtil.isEmpty(dateFormat)) {
-            dateFormat = FORMAT_DATE_TIME;
+            dateFormat = FORMAT_DATE;
         }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, Locale.CHINESE);
         if (!XCStringUtil.isEmpty(timeZone)) {
